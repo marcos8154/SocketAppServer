c5f5383af8aca1ff3d6edc0bf2e8ac98773f4177
Marcos8154 <marcos8154@gmail.com>
2022-06-23 14:25:55 -0300

    update client version

--- MobileAppServerClient/Properties/AssemblyInfo.cs
+++ MobileAppServerClient/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
 // usando o '*' como mostrado abaixo:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.1.11.1301")]
-[assembly: AssemblyFileVersion("2.1.11.1301")]
+[assembly: AssemblyVersion("2.1.12.1301")]
+[assembly: AssemblyFileVersion("2.1.12.1301")]
